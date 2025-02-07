adcf943d37fa62efb3beceef3471d424d367a6aa
ikalathifah <ikalathifah24@gmail.com>
2025-02-07 14:30:49 +0700

    js-1

---
+++ javascript-myskill/js-1/js-1.js
@@ -0,0 +1,19 @@
+// Example 
+var y;
+y = 7;
+
+document.getElementById('example').innerText = y
+// End of Example
+
+//Case 1
+// write your code here
+
+
+document.getElementById('Case1').innerText = x;
+// End of Case 1
+
+//Case 2
+// write your code here
+
+document.getElementById('Case2').innerText = a;
+// End of Case 2
