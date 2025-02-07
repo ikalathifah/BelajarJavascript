790494695bd95f4254bd2d113d81965858c56a0b
ikalathifah <ikalathifah24@gmail.com>
2025-02-07 17:07:55 +0700

    js-2

---
+++ javascript-myskill/js-2/js-2.js
@@ -0,0 +1,18 @@
+// Example 
+let x;
+x = "example";
+
+document.getElementById('example').innerText = x
+// End of Example
+
+//Case 1
+let y;
+y = true;
+
+document.getElementById('Case1').innerText = y;
+// End of Case 1
+
+//Case 2
+const a = typeof(y);
+document.getElementById('Case2').innerText = a;
+// End of Case 2
