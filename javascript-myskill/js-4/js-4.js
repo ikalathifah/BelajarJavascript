937c05966ccb4952bb022309c9c723221c3b6df4
ikalathifah <ikalathifah24@gmail.com>
2025-02-08 15:20:55 +0700

    js-4

---
+++ javascript-myskill/js-4/js-4.js
@@ -0,0 +1,23 @@
+// Example 
+const person = {
+    name: 'Zaky',
+    age: 29
+  }
+  const y = person.age
+  document.getElementById('example').innerText = "the age is " + y
+  // End of Example
+  
+  //Case 1
+  const car = {
+    manufacture: 'hondi',
+    year: 2022
+  }
+  const x = car.manufacture
+  document.getElementById('Case1').innerText = "the car manufacture is " + x;
+  // End of Case 1
+  
+  //Case 2
+  array = [1, 5, 7, 10, 22, 25]
+  const a = array[3];
+  document.getElementById('Case2').innerText = a;
+  // End of Case 2
