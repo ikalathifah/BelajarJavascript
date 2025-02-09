f0444fce3d9d25d1d9c8083e5f045a835da74759
ikalathifah <ikalathifah24@gmail.com>
2025-02-09 13:31:01 +0700

    js-9

---
+++ javascript-myskill/js-9/js-9.js
@@ -0,0 +1,21 @@
+// Example 
+{
+    function multiply() {
+        return 2 * 3;
+    }
+
+    document.getElementById("example").innerText = multiply()
+}
+// expected result = 6
+// End of Example
+{
+    //Case 1
+    // write your code here
+    function multiply(x, y) {
+        return x * y;
+    }
+
+    document.getElementById("Case1").innerText = multiply(3, 7) 
+    // expected result 21
+    // End of Case 1
+}
