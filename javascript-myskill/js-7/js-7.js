79ac8a8abe775891af099ad3794b9796ee27d6e3
ikalathifah <ikalathifah24@gmail.com>
2025-02-09 10:11:38 +0700

    js-7

---
+++ javascript-myskill/js-7/js-7.js
@@ -0,0 +1,18 @@
+// Example 
+{
+    for (let i = 0; i <= 3; i++) {
+        document.getElementById("example").innerText += i
+    }
+}
+// expected result = 0123
+// End of Example
+{
+//Case 1
+    let i = 0
+    while (i<5) {
+        document.getElementById("Case1").innerText += i;
+        i++;
+    }
+// expected result = 01234
+// End of Case 1
+}
